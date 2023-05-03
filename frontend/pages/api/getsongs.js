6d47f08405efafb8dfe73cd3dbc44cfe5f60d635
Rishik Sarkar <rs1894@scarletmail.rutgers.edu>
2023-05-03 18:27:13 -0400

    song retrieval functionality added

--- frontend/pages/api/getsongs.js
+++ frontend/pages/api/getsongs.js
@@ -17,18 +17,29 @@ export default async function handler(req, res) {
     });
 
     try {
+        if (body.user == '' || body.user == null)
+        {
+            res.status(401).json({ error: 'Please Login or Register!' });
+            return;
+        }
+
         const idquery = "SELECT id FROM account WHERE username = ?";
-        const id = [body.username];
-        const [data] = await dbconnection.execute(idquery, id);
+        const user = [body.user];
+        const [data] = await dbconnection.execute(idquery, user);
 
         if (data.length > 0) {
             const songquery = "SELECT trackname, trackartists FROM songlist WHERE uid = ?";
-            const uid = data[0];
-            const [songList] = await dbconnection.execute(songquery, uid);
-            
-            res.status(200).json({ songs: songList });
-            
-        } else {
+            const songs = [data[0].id];
+            const [songList] = await dbconnection.execute(songquery, songs);
+
+            if (songList.length > 0) {
+                res.json({ songs: songList });
+            }
+            else {
+                res.status(401).json({ error: 'No Songs Added Yet!' });
+            }
+        }
+        else {
             res.status(401).json({ error: 'Please Login or Register!' });
         }
     } catch (error) {
