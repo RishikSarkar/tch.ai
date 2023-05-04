969de721046bd9d45f269e5d16ca719d1a82e57a
Rishik Sarkar <rs1894@scarletmail.rutgers.edu>
2023-05-04 19:18:09 -0400

    added dashboard page

--- frontend/pages/Dashboard.jsx
+++ frontend/pages/Dashboard.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { FiPlay } from 'react-icons/fi';
-import { user } from './Navbar';
+import { user } from '../components/Navbar';
 
 const Dashboard = () => {
 
-    const [songList, setSongList] = useState(null);
+    const [songList, setSongList] = useState([]);
 
     useEffect(() => {
         handleGetSong();
@@ -22,10 +22,10 @@ const Dashboard = () => {
             });
 
             const data = await response.json();
-            setSongList(data);
 
             if (response.ok) {
                 console.log(data.message);
+                setSongList(data.songs);
             } else {
                 alert(data.error);
             }
@@ -36,18 +36,17 @@ const Dashboard = () => {
     };
 
     return (
-        <div id='songs' className='font-roboto selection:text-black selection:bg-white/50 w-full h-screen bg-white text-center'>
+        <div id='dashboard' className='font-roboto selection:text-black selection:bg-white/50 w-full h-screen text-center'>
             <div className='max-w-[1440px] w-full h-full mx-auto p-12 flex justify-center items-center'>
                 <div className='-mb-20 mt-12 md:mt-0'>
                     <div className='max-w-[400px] md:max-w-none bg-white/5 p-4'>
-                        <div className={`border-2 border-custom p-6`} style={{ '--border-color': `var(--bg-end)` }} >
+                        <div className={`border-2 border-custom p-6 max-h-[400px] overflow-y-scroll`} style={{ '--border-color': `var(--bg-end)` }} >
                             <table className='font-light text-white'>
                                 <tbody>
-                                    {songList.map((song, index) => (
+                                    {(songList.length > 0) && songList.map((song, index) => (
                                         <tr key={index} className='border-2 border-white/10'>
                                             <td className='px-4 py-2'>
-                                                <button className={(predictionString == 'sad' || predictionString == 'angry' || predictionString == 'surprise') ? 'p-4 hover:bg-white/20 ease-in duration-100 rounded-full hover:text-white/80'
-                                                    : `p-4 hover:bg-white/20 ease-in duration-100 rounded-full hover:text-custom`}
+                                                <button className={`p-4 hover:bg-white/20 ease-in duration-100 rounded-full hover:text-custom`}
                                                     style={{ '--border-color': `var(--bg-end)` }}>
                                                     <FiPlay size={20} />
                                                 </button>
